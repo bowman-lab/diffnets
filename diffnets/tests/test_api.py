9a252b2ccca46506d15200a76fbccb8c4b18e7ea
Michael Ward <mickward1029@gmail.com>
2020-11-24 13:33:56 -0600

    Added test to ensure whitened trajectories have covariance matrices with 1 across the diagonals

--- diffnets/tests/test_api.py
+++ diffnets/tests/test_api.py
@@ -2,8 +2,10 @@ import os
 import shutil
 import mdtraj as md
 import tempfile
+import numpy as np
 
-from diffnets.data_processing import ProcessTraj
+from diffnets.utils import get_fns
+from diffnets.data_processing import ProcessTraj, WhitenTraj
 
 CURR_DIR = os.getcwd()
 UP_DIR = CURR_DIR[:-len(CURR_DIR.split('/')[-1])]
@@ -29,3 +31,27 @@ def test_default_inds():
 
     finally:
         shutil.rmtree(td)
+
+def test_whitening_correctness():
+
+    w = WhitenTraj("./data/whitened/")
+    master = md.load("./data/whitened/master.pdb")
+    traj1 = md.load("./data/whitened/aligned_xtcs/000000.xtc",top=master)
+    traj2 = md.load("./data/whitened/aligned_xtcs/000001.xtc",top=master)
+    wm = np.load("./data/whitened/wm.npy")
+    w.apply_whitening_xtc_dir(w.xtc_dir,master.top,wm,
+                              w.cm,1,"./data/whitened/whitened_xtcs")
+    traj_fns = get_fns("./data/whitened/whitened_xtcs/", "*.xtc")
+    traj = md.load(traj_fns[0],top=master)
+    coords = traj.xyz.reshape((2501, 3 * 39))
+    c00_1 = np.matmul(coords.transpose(),coords)
+    
+    traj = md.load(traj_fns[1],top=master)
+    coords = traj.xyz.reshape((2500, 3 * 39))
+    c00_2 = np.matmul(coords.transpose(),coords)
+    c00 = c00_1 + c00_2
+    c00 /= 5001
+
+    assert (np.abs(117 - np.sum(np.diagonal(c00))) < 1)
+
+
